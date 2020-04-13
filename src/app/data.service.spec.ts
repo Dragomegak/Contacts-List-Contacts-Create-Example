99af0bf5eacb8a18a9ca61f263b9c5bde4e21cc4
Dragomegak <stevenle6569@gmail.com>
2020-04-13 18:10:37 -0400

    setting up service data in src/app/data.service.ts

--- src/app/data.service.spec.ts
+++ src/app/data.service.spec.ts
@@ -1,16 +1,23 @@
-import { TestBed } from '@angular/core/testing';
+import { Injectable } from '@angular/core';
 
-import { DataService } from './data.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
 
-describe('DataService', () => {
-  let service: DataService;
+  contacts = [
+    {id: 1, name: "Contact 001", description: "Contact 001 des", email: "c001@email.com"},
+    {id: 2, name: "Contact 002", description: "Contact 002 des", email: "c002@email.com"},
+    {id: 3, name: "Contact 003", description: "Contact 003 des", email: "c003@email.com"},
+    {id: 4, name: "Contact 004", description: "Contact 004 des", email: "c004@email.com"}
+  ];
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(DataService);
-  });
+  constructor() { }
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
-});
+  public getContacts():Array<{id, name, description, email}>{
+    return this.contacts;
+  }
+  public createContact(contact: {id, name, description, email}){
+    this.contacts.push(contact);
+  }
+}

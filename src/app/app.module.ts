f902b057368d478b246b758b4d9b949eef45ef8a
Dragomegak <stevenle6569@gmail.com>
2020-04-14 18:07:24 -0400

    added header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ContactCreateComponent } from './contact-create/contact-create.component';
 import { ContactListComponent } from './contact-list/contact-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     ContactCreateComponent,
-    ContactListComponent
+    ContactListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,

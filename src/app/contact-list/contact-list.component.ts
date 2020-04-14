906a5be3a54f7ff216836a88536e6c6333a2eea7
Dragomegak <stevenle6569@gmail.com>
2020-04-14 18:47:34 -0400

    added inner workings towards contacts listing page and create contacts page

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-contact-list',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactListComponent implements OnInit {
 
-  constructor() { }
+  contacts ;
+  selectedContact;
+  constructor(public dataService: DataService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.contacts = this.dataService.getContacts();    
+  }
+  public selectContact(contact){
+    this.selectedContact = contact;
   }
 
 }
